ba89c22df3e45323d8bbcf1482a624531afce01c
Alexander Spetko <a.spetko@gmail.com>
2017-11-03 07:11:54 +0100

    First copy

---
+++ js/database copy.js
@@ -0,0 +1,635 @@
+function getFestivalPlan() {
+  return {
+    "festival": {
+      "the5": {
+        "AM1000": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space",
+            "Genres": [
+              "Action",
+              "Comedy",
+              "Horror"
+            ]
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of AM1000
+        "PM1200": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM1200
+        "PM0200": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0200
+        "PM0400": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0400
+        "PM0600": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0600
+        "PM0800": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0800
+        "PM1000": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM1000
+      },// End of the 5th
+      "the6": {
+        "AM1000": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of AM1000
+        "PM1200": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM1200
+        "PM0200": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0200
+        "PM0400": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0400
+        "PM0600": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0600
+        "PM0800": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0800
+        "PM1000": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM1000
+      },// End of the 6th
+      "the7": {
+        "AM1000": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of AM1000
+        "PM1200": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM1200
+        "PM0200": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0200
+        "PM0400": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0400
+        "PM0600": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0600
+        "PM0800": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0800
+        "PM1000": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM1000
+      },// End of the 7th
+      "the8": {
+        "AM1000": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of AM1000
+        "PM1200": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM1200
+        "PM0200": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0200
+        "PM0400": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0400
+        "PM0600": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0600
+        "PM0800": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM0800
+        "PM1000": [
+          {
+            "ID": "51",
+            "Movie": "Bambi's Revenege",
+            "Area": "Open Space"
+          },
+          {
+            "ID": "52",
+            "Movie": "Bambi Returns",
+            "Area": "Near the wood"
+          },
+          {
+            "ID": "53",
+            "Movie": "From Dusk till Dawn - Bambi in the world of Happiness",
+            "Area": "Dutsy Field"
+          },
+          {
+            "ID": "54",
+            "Movie": "Dragon's Lagoon",
+            "Area": "Open Space"
+          }
+        ], // End of PM1000
+      },// End of the 8th
+    } // End of festival
+  }; // return
+}
